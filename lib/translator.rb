0935ecd6f1c81975253aa629e7eb863962dfbc7d
drubaby <peryton6@gmail.com>
2018-08-20 16:32:16 -0400

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,30 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticons = YAML.load_file(path) #works
+
+  lib_hash = {
+    "get_emoticon" => {},
+    "get_meaning" => {}
+  }
+
+  emoticons.each do |meaning, symbols|
+    lib_hash["get_meaning"][symbols[1]] = meaning
+    lib_hash["get_emoticon"][symbols[0]] = symbols[1]
+  end
+
+  lib_hash
+  binding.pry
+=begin
+{"get_emoticon"=>
+  {"O:)"=>"☜(⌒▽⌒)☞",
+   ">:("=>"ヽ(ｏ`皿′ｏ)ﾉ"} etc etc
+ {"get_meaning"=>
+     {"☜(⌒▽⌒)☞"=>"angel",
+      "ヽ(ｏ`皿′ｏ)ﾉ"=>"angry"} etc etc
+=end
 end
 
 def get_japanese_emoticon
